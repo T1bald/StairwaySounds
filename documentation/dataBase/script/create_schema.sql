36c2a6c0bf33073b7d9f4039a65b94a65fa4f105
bbuallbest <bbuallbest@gmail.com>
2014-11-14 13:24:08 +0200

    Changed database schema

--- documentation/dataBase/script/create_schema.sql
+++ documentation/dataBase/script/create_schema.sql
@@ -5,8 +5,8 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema stairway_sounds
 -- -----------------------------------------------------
---
---
+-- 
+-- 
 DROP SCHEMA IF EXISTS `stairway_sounds` ;
 CREATE SCHEMA IF NOT EXISTS `stairway_sounds` DEFAULT CHARACTER SET utf8 ;
 USE `stairway_sounds` ;
@@ -19,8 +19,11 @@ DROP TABLE IF EXISTS `stairway_sounds`.`roles` ;
 CREATE TABLE IF NOT EXISTS `stairway_sounds`.`roles` (
   `id_role` INT NOT NULL,
   `name` VARCHAR(45) NULL,
+  `deleted` BIT(1) NULL DEFAULT 0,
+  `creating_timestamp` TIMESTAMP NULL,
+  `last_changing_timestamp` TIMESTAMP NULL,
   PRIMARY KEY (`id_role`))
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -34,15 +37,18 @@ CREATE TABLE IF NOT EXISTS `stairway_sounds`.`users` (
   `username` VARCHAR(45) NULL,
   `pass_hash` VARCHAR(45) NULL,
   `pass_salt` VARCHAR(45) NULL,
-  `role_id` INT NOT NULL,
+  `role_id` INT NULL,
+  `deleted` BIT(1) NULL DEFAULT 0,
+  `creating_timestamp` TIMESTAMP NULL,
+  `last_changing_timestamp` TIMESTAMP NULL,
   PRIMARY KEY (`id_user`),
   INDEX `fk_role_id_idx` (`role_id` ASC),
   CONSTRAINT `use_fk_role_id`
-    FOREIGN KEY (`role_id`)
-    REFERENCES `stairway_sounds`.`roles` (`id_role`)
+  FOREIGN KEY (`role_id`)
+  REFERENCES `stairway_sounds`.`roles` (`id_role`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -54,8 +60,11 @@ CREATE TABLE IF NOT EXISTS `stairway_sounds`.`artists` (
   `id_artist` INT NOT NULL,
   `pseudonym` VARCHAR(45) NULL,
   `description` TEXT NULL,
+  `deleted` BIT(1) NULL DEFAULT 0,
+  `creating_timestamp` TIMESTAMP NULL,
+  `last_changing_timestamp` TIMESTAMP NULL,
   PRIMARY KEY (`id_artist`))
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -69,14 +78,17 @@ CREATE TABLE IF NOT EXISTS `stairway_sounds`.`albums` (
   `description` TEXT NULL,
   `cover` BLOB NULL,
   `artist_id` INT NULL,
+  `deleted` BIT(1) NULL DEFAULT 0,
+  `creating_timestamp` TIMESTAMP NULL,
+  `last_changing_timestamp` TIMESTAMP NULL,
   PRIMARY KEY (`id_album`),
   INDEX `fk_artist_id_idx` (`artist_id` ASC),
   CONSTRAINT `alb_fk_artist_id`
-    FOREIGN KEY (`artist_id`)
-    REFERENCES `stairway_sounds`.`artists` (`id_artist`)
+  FOREIGN KEY (`artist_id`)
+  REFERENCES `stairway_sounds`.`artists` (`id_artist`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -86,9 +98,12 @@ DROP TABLE IF EXISTS `stairway_sounds`.`raitings` ;
 
 CREATE TABLE IF NOT EXISTS `stairway_sounds`.`raitings` (
   `id_raiting` INT NOT NULL,
-  `downloads` MEDIUMTEXT NULL,
+  `downloads` INT NULL,
+  `deleted` BIT(1) NULL DEFAULT 0,
+  `creating_timestamp` TIMESTAMP NULL,
+  `last_changing_timestamp` TIMESTAMP NULL,
   PRIMARY KEY (`id_raiting`))
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -102,8 +117,11 @@ CREATE TABLE IF NOT EXISTS `stairway_sounds`.`tracks_additional_info` (
   `language` VARCHAR(45) NULL,
   `year_of_issue` VARCHAR(45) NULL,
   `label` VARCHAR(45) NULL,
+  `deleted` BIT(1) NULL DEFAULT 0,
+  `creating_timestamp` TIMESTAMP NULL,
+  `last_changing_timestamp` TIMESTAMP NULL,
   PRIMARY KEY (`id_additional_info`))
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -114,32 +132,35 @@ DROP TABLE IF EXISTS `stairway_sounds`.`tracks` ;
 CREATE TABLE IF NOT EXISTS `stairway_sounds`.`tracks` (
   `id_track` INT NOT NULL,
   `track_name` VARCHAR(45) NULL,
-  `file_path` MEDIUMTEXT NOT NULL,
+  `file_path` VARCHAR(100) NOT NULL,
   `duration` MEDIUMTEXT NULL,
   `description` TEXT NULL,
   `album_id` INT NULL,
   `raiting_id` INT NULL,
   `track_info_id` INT NULL,
+  `deleted` BIT(1) NULL DEFAULT 0,
+  `creating_timestamp` TIMESTAMP NULL,
+  `last_changing_timestamp` TIMESTAMP NULL,
   PRIMARY KEY (`id_track`),
   INDEX `fk_album_id_idx` (`album_id` ASC),
   INDEX `fk_raiting_id_idx` (`raiting_id` ASC),
   INDEX `fk_tracks_tracks_additional_info1_idx` (`track_info_id` ASC),
   CONSTRAINT `tra_fk_album_id`
-    FOREIGN KEY (`album_id`)
-    REFERENCES `stairway_sounds`.`albums` (`id_album`)
+  FOREIGN KEY (`album_id`)
+  REFERENCES `stairway_sounds`.`albums` (`id_album`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `tra_fk_raiting_id`
-    FOREIGN KEY (`raiting_id`)
-    REFERENCES `stairway_sounds`.`raitings` (`id_raiting`)
+  FOREIGN KEY (`raiting_id`)
+  REFERENCES `stairway_sounds`.`raitings` (`id_raiting`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_tracks_tracks_additional_info1`
-    FOREIGN KEY (`track_info_id`)
-    REFERENCES `stairway_sounds`.`tracks_additional_info` (`id_additional_info`)
+  FOREIGN KEY (`track_info_id`)
+  REFERENCES `stairway_sounds`.`tracks_additional_info` (`id_additional_info`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -150,8 +171,11 @@ DROP TABLE IF EXISTS `stairway_sounds`.`access_modifires` ;
 CREATE TABLE IF NOT EXISTS `stairway_sounds`.`access_modifires` (
   `id_access_modifire` INT NOT NULL,
   `name` VARCHAR(45) NULL,
+  `deleted` BIT(1) NULL DEFAULT 0,
+  `creating_timestamp` TIMESTAMP NULL,
+  `last_changing_timestamp` TIMESTAMP NULL,
   PRIMARY KEY (`id_access_modifire`))
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -165,20 +189,23 @@ CREATE TABLE IF NOT EXISTS `stairway_sounds`.`playlists` (
   `description` TEXT NULL,
   `access_modifier_id` INT NOT NULL,
   `user_id` INT NULL,
+  `deleted` BIT(1) NULL DEFAULT 0,
+  `creating_timestamp` TIMESTAMP NULL,
+  `last_changing_timestamp` TIMESTAMP NULL,
   PRIMARY KEY (`id_playlist`),
   INDEX `fk_access_modifier_id_idx` (`access_modifier_id` ASC),
   INDEX `fk_user_id_idx` (`user_id` ASC),
   CONSTRAINT `pla_fk_access_modifier_id`
-    FOREIGN KEY (`access_modifier_id`)
-    REFERENCES `stairway_sounds`.`access_modifires` (`id_access_modifire`)
+  FOREIGN KEY (`access_modifier_id`)
+  REFERENCES `stairway_sounds`.`access_modifires` (`id_access_modifire`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `pla_fk_user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `stairway_sounds`.`users` (`id_user`)
+  FOREIGN KEY (`user_id`)
+  REFERENCES `stairway_sounds`.`users` (`id_user`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -193,16 +220,16 @@ CREATE TABLE IF NOT EXISTS `stairway_sounds`.`tracks_in_playlists` (
   INDEX `fk_track_id_idx` (`track_id` ASC),
   INDEX `fk_playlist_id_idx` (`playlist_id` ASC),
   CONSTRAINT `tip_fk_playlist_id`
-    FOREIGN KEY (`playlist_id`)
-    REFERENCES `stairway_sounds`.`playlists` (`id_playlist`)
+  FOREIGN KEY (`playlist_id`)
+  REFERENCES `stairway_sounds`.`playlists` (`id_playlist`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `tip_fk_track_id`
-    FOREIGN KEY (`track_id`)
-    REFERENCES `stairway_sounds`.`tracks` (`id_track`)
+  FOREIGN KEY (`track_id`)
+  REFERENCES `stairway_sounds`.`tracks` (`id_track`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -214,8 +241,11 @@ CREATE TABLE IF NOT EXISTS `stairway_sounds`.`hashtags` (
   `id_hashtag` INT NOT NULL,
   `name` VARCHAR(45) NULL,
   `description` TEXT NULL,
+  `deleted` BIT(1) NULL DEFAULT 0,
+  `creating_timestamp` TIMESTAMP NULL,
+  `last_changing_timestamp` TIMESTAMP NULL,
   PRIMARY KEY (`id_hashtag`))
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -230,16 +260,16 @@ CREATE TABLE IF NOT EXISTS `stairway_sounds`.`tracks_have_hashtags` (
   INDEX `thh_fk_hashtag_id_idx` (`hashtag_id` ASC),
   INDEX `thh_fk_track_id_idx` (`track_id` ASC),
   CONSTRAINT `thh_fk_track_id`
-    FOREIGN KEY (`track_id`)
-    REFERENCES `stairway_sounds`.`tracks` (`id_track`)
+  FOREIGN KEY (`track_id`)
+  REFERENCES `stairway_sounds`.`tracks` (`id_track`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `thh_fk_hashtag_id`
-    FOREIGN KEY (`hashtag_id`)
-    REFERENCES `stairway_sounds`.`hashtags` (`id_hashtag`)
+  FOREIGN KEY (`hashtag_id`)
+  REFERENCES `stairway_sounds`.`hashtags` (`id_hashtag`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -254,16 +284,16 @@ CREATE TABLE IF NOT EXISTS `stairway_sounds`.`users_subscribe_hashtags` (
   INDEX `ush_fk_user_id_idx` (`user_id` ASC),
   INDEX `ush_fk_hashtag_id_idx` (`hashtag_id` ASC),
   CONSTRAINT `ush_fk_hashtag_id`
-    FOREIGN KEY (`hashtag_id`)
-    REFERENCES `stairway_sounds`.`hashtags` (`id_hashtag`)
+  FOREIGN KEY (`hashtag_id`)
+  REFERENCES `stairway_sounds`.`hashtags` (`id_hashtag`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `ush_fk_user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `stairway_sounds`.`users` (`id_user`)
+  FOREIGN KEY (`user_id`)
+  REFERENCES `stairway_sounds`.`users` (`id_user`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -274,9 +304,12 @@ DROP TABLE IF EXISTS `stairway_sounds`.`styles` ;
 CREATE TABLE IF NOT EXISTS `stairway_sounds`.`styles` (
   `id_style` INT NOT NULL,
   `style_name` VARCHAR(45) NOT NULL,
+  `deleted` BIT(1) NULL DEFAULT 0,
+  `creating_timestamp` TIMESTAMP NULL,
+  `last_changing_timestamp` TIMESTAMP NULL,
   PRIMARY KEY (`id_style`),
   UNIQUE INDEX `style_name_UNIQUE` (`style_name` ASC))
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -285,22 +318,22 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `stairway_sounds`.`tracks_have_styles` ;
 
 CREATE TABLE IF NOT EXISTS `stairway_sounds`.`tracks_have_styles` (
-  `tracks_id_track` INT NOT NULL,
-  `styles_id_style` INT NOT NULL,
-  INDEX `fk_tracks_have_styles_tracks1_idx` (`tracks_id_track` ASC),
-  INDEX `fk_tracks_have_styles_styles1_idx` (`styles_id_style` ASC),
-  PRIMARY KEY (`tracks_id_track`, `styles_id_style`),
-  CONSTRAINT `fk_tracks_have_styles_tracks1`
-    FOREIGN KEY (`tracks_id_track`)
-    REFERENCES `stairway_sounds`.`tracks` (`id_track`)
+  `track_id` INT NOT NULL,
+  `style_id` INT NOT NULL,
+  INDEX `ths_fk_track_id_idx` (`track_id` ASC),
+  INDEX `ths_fk_style_id_idx` (`style_id` ASC),
+  PRIMARY KEY (`track_id`, `style_id`),
+  CONSTRAINT `ths_fk_track_id`
+  FOREIGN KEY (`track_id`)
+  REFERENCES `stairway_sounds`.`tracks` (`id_track`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tracks_have_styles_styles1`
-    FOREIGN KEY (`styles_id_style`)
-    REFERENCES `stairway_sounds`.`styles` (`id_style`)
+  CONSTRAINT `ths_fk_style_id`
+  FOREIGN KEY (`style_id`)
+  REFERENCES `stairway_sounds`.`styles` (`id_style`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
