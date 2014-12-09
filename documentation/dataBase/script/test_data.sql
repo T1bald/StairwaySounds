d95f3a1aa45135ef452370d0692f73c295e15838
Matvey <matvey.mtn@gmail.com>
2014-12-09 22:26:45 +0200

    Save DataBase Schema And Script Changes

---
+++ documentation/dataBase/script/test_data.sql
@@ -0,0 +1,23 @@
+INSERT INTO `stairway_sounds`.`roles`
+(`id_role`,
+`name`)
+VALUES
+(1,
+'ADMIN');
+
+INSERT INTO `stairway_sounds`.`users`
+(`id_user`,
+`email`,
+`username`,
+`pass_hash`,
+`pass_salt`,
+`password`
+)
+VALUES
+(1,
+'email',
+'uName',
+'pHash',
+'pSalt',
+'MyPassword'
+);
