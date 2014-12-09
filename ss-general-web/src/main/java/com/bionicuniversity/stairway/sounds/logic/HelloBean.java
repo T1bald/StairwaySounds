30cae2ddc6056a696c65362c1f681b678b9107ce
Matvey <matvey.mtn@gmail.com>
2014-12-10 01:58:26 +0200

    Added Basic JSF Configurations.

---
+++ ss-general-web/src/main/java/com/bionicuniversity/stairway/sounds/logic/HelloBean.java
@@ -0,0 +1,34 @@
+package com.bionicuniversity.stairway.sounds.logic;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+/**
+ * Created by root on 10.12.14.
+ */
+
+@Named
+@RequestScoped
+public class HelloBean implements Serializable{
+
+    private String message = "Hello From Managed Bean!";
+
+    private String name;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
